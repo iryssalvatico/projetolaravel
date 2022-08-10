04d9ecb0dd1b6b267611321e00fc070525f02842
Alunos <iryssalvatico16@gmail.com>
2022-08-10 09:56:42 -0300

    aula10ag

--- app/Http/Controllers/MensagemController.php
+++ app/Http/Controllers/MensagemController.php
@@ -6,6 +6,7 @@ use App\Models\Mensagem;
 use Illuminate\Http\Request;
 use App\Models\Topico;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class MensagemController extends Controller
 {
@@ -42,16 +43,20 @@ class MensagemController extends Controller
         $validated = $request->validate([
             'titulo' => 'required|max:255',
             'mensagem' => 'required|max:255',
-            'topico' => 'array|exists:App\Models\Topico,id'
+            'topico' => 'array|exists:App\Models\Topico,id',
+            'imagem' => 'image'
         ]);
         if ($validated){
             $mensagem = new Mensagem();
             $mensagem->user_id = Auth::User()->id;
             $mensagem->titulo = $request->get('titulo');
             $mensagem->mensagem = $request->get('mensagem');
+            $name = $request->file('imagem')->getClientOriginalName();
+            $path = $request->file('imagem')->storeAs("public/img", $name);
+            $mensagem->imagem = $path;
             $mensagem->save();
             $mensagem->topicos()->attach($request->get('topico'));
-            //return redirect('mensagem');
+            return redirect('mensagem');
         }
     }
 
@@ -90,11 +95,15 @@ class MensagemController extends Controller
         $validated = $request->validate([
             'titulo' => 'required|max:255',
             'mensagem' => 'required|max:255',
-            'topico' => 'array|exists:App\Models\Topico,id'
+            'topico' => 'array|exists:App\Models\Topico,id',
+            'imagem' => 'image'
         ]);
         if ($validated){
             $mensagem->titulo = $request->get('titulo');
             $mensagem->mensagem = $request->get('mensagem');
+            $name = $request->file('imagem')->getClientOriginalName();
+            $path = $request->file('imagem')->storeAs("public/img", $name);
+            $mensagem->imagem = $path;
             $mensagem->save();
             $mensagem->topicos()->sync($request->get('topicos'));
             return redirect('mensagem');
