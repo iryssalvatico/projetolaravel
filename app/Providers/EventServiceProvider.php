8487c2c2f446b3a52f83ac577639a75fe25755ca
iryssalvatico <iryssalvatico16@gmail.com>
2022-10-05 09:51:38 -0300

    aula5-10

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
@@ -27,7 +29,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::observe(UserObserver::class);
     }
 
     /**
