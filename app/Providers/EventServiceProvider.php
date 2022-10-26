d836bd25fd53ad6d5919a1a4173f649317adfcab
iryssalvatico <iryssalvatico16@gmail.com>
2022-10-26 09:40:37 -0300

    aula 26-10

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
 use App\Models\User;
 use App\Observers\UserObserver;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
@@ -20,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        'App\Events\AvisoUserCreated' => [
+            'App\Listeners\SendPushListener'
+        ]
     ];
 
     /**
